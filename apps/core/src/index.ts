cad20bf37d8c0826007fc055a1a3010f668d92e6
Sayeedur Rahman <sayeedurrahman442@gmail.com>
2024-09-17 09:06:05 +0600

    config: watch changes

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -5,7 +5,11 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
-  });
+});
+
+app.get('/hello-world', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
