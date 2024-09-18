b3e86dfe5c31d99cdadfcf7a8ff2a5d0670b2e8a
Sayeedur Rahman <97797507+avonoeia@users.noreply.github.com>
2024-09-18 17:40:24 +0600

    chore(cors)\!: setup cors BREAKING CHANGE: cors setup to only allow localhost:5173

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 const port = process.env.PORT || 3000
 
 const app = express()
+
+// Configurations
+app.use(express.json())
+app.use(cors({
+    origin: 'http://127.0.0.1:5173',
+    optionsSuccessStatus: 200
+}))
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
